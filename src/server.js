1319d0db7d1b0bd2b8b3deb5bcc3e7e8e9631590
Evan Hallmark <ehallmar@usc.edu>
2022-01-14 09:53:02 -0800

    pedersen

--- src/server.js
+++ src/server.js
@@ -6,26 +6,27 @@ const { version } = require('../package.json')
 const { isAddress } = require('web3-utils')
 const { setup } = require('./relayer')
 
-const app = express()
-app.use(express.json())
-
-// Add CORS headers
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next()
-})
-
-// Log error to console but don't send it to the client to avoid leaking data
-app.use((err, req, res, next) => {
-  if (err) {
-    console.error(err)
-    return res.sendStatus(500)
-  }
-  next()
-})
-
 setup().then(()=>{
+  const app = express()
+  app.use(express.json())
+
+  // Add CORS headers
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    next()
+  })
+
+  // Log error to console but don't send it to the client to avoid leaking data
+  app.use((err, req, res, next) => {
+    if (err) {
+      console.error(err)
+      return res.sendStatus(500)
+    }
+    next()
+  })
+
   app.get('/', status.status)
   app.get('/status', status.status)
   app.get('/fees', status.fees)
